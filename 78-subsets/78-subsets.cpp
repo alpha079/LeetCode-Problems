c3e175a2b60cd9f559bddf11933c063b0212fbb9
Shivam Soni <shivamkmv2311@gmail.com>
2022-02-13 12:36:30 +0530

    Time: 7 ms (26.93%), Space: 7 MB (85.44%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
-  void helper(vector<int>& nums, int idx,vector<int>& ans, vector<vector<int>>& res)
+    
+   
+    vector<vector<int>> generate(vector<int>& nums, int size)
     {
-       res.push_back(ans);
-      for(int i=idx;i<nums.size();i++)
-      {
-          ans.push_back(nums[i]);
-          helper(nums,i+1,ans,res);
-          ans.pop_back();
-          
-      }
-      
         
+        int subCnt=(1<<size); //2*size 1 left shift 2
+        
+        vector<vector<int>> res(subCnt);
+        for(int mask=0; mask<subCnt;++mask)
+        {
+           
+            for(int i=0;i<size;++i)
+            {
+                cout<<i<<" ";
+                if((mask &(1<<i))!=0)
+                {
+                    res[mask].push_back(nums[i]);
+                }
+            }
+            cout<<endl;
+            
+            
+        }
+        return res;
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> ans;
         
-        helper(nums,0,ans,res);
+    
         
-        return res;
+        return generate(nums,nums.size());
+       
         
     }
 };
