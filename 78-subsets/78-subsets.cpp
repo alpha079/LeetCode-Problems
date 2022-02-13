7d76d9aff8b04c487ff2cd0ac75e5339d41240c0
Shivam Soni <shivamkmv2311@gmail.com>
2022-02-13 11:06:18 +0530

    Time: 0 ms (100.00%), Space: 7 MB (95.79%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     
+       
+    //TC: O(n(2 ^ n)). 
+//For every index i two recursive cases originate, So Time Complexity is O(2^n). If we include the time taken to copy the subset vector into the res vector the time taken will be equal to the size of the subset vector.
+    //Blog for TC:- https://medium.com/@vasanths294/permutation-combination-subset-time-complexity-eca924e00071
     void helper(vector<int>& nums, int idx,vector<int>& ans, vector<vector<int>>& res)
     {
         if(idx==nums.size())
         {
-            res.push_back(ans);
+            res.push_back(ans); //Copy vector takes O(N) time max for full length
             return;
         }
         ans.push_back(nums[idx]);
