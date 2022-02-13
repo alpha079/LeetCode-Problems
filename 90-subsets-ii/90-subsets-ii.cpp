4702f30c0a4b222f7fc1793cefffa5488239ae0e
Shivam Soni <shivamkmv2311@gmail.com>
2022-02-13 13:27:14 +0530

    Time: 23 ms (5.31%), Space: 8.2 MB (35.81%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    //TC:- O(N*2^N) + O(Nlogn) for sorting
-    void helper(vector<int>& nums, int idx,vector<vector<int>>& res,vector<int>& ans)
-    {
-         res.push_back(ans);
-        for(int i=idx;i<nums.size();i++)
-        {
-            if(i==idx || nums[i]!=nums[i-1])
-            {
-                ans.push_back(nums[i]);
-                  helper(nums,i+1,res,ans);
-                  ans.pop_back();
-            }
-          
-        }
-        
-    }
+    
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        unordered_set<string> hash;
+        int size=(1<<n);
         
-        
-        sort(nums.begin(),nums.end());
-        
-        vector<vector<int>> res;
-        vector<int> ans;
-        
-        helper(nums,0,res,ans);
-        return res;
-        
-        
+        for(int mask=0;mask<size;mask++)////O(2^N)
+        {
+            vector<int> sub;
+            string temp="";
+            for(int i=0;i<nums.size();i++)  //O(N)
+            {
+                if((mask & (1<<i))!=0)
+                {
+                    sub.push_back(nums[i]);
+                    temp+=to_string(nums[i])+ ",";
+                }
+            }
+            if(!hash.count(temp))
+            {
+                ans.push_back(sub);
+                hash.insert(temp);
+            }
+        }
+        return ans;
     }
 };
