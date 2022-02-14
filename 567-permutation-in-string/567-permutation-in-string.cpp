25e038e84c5b54cff02bbd1ff6fe5a32d263fb53
Shivam Soni <shivamkmv2311@gmail.com>
2022-02-14 18:06:12 +0530

    Time: 7 ms (82.78%), Space: 7.2 MB (72.58%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -2,6 +2,14 @@ class Solution {
 public:
     // Time complexity: O(n * 26) = O(n)
 // Time space: O(26 * 2) = O(1)
+    
+    bool areVectorEqual(vector<int>&a, vector<int>& b)
+    {
+        for(int i=0; i<26; i++){
+            if(a[i]!=b[i]) return false;
+        }
+        return true;
+    }
     bool checkInclusion(string s1, string s2) {
        int m=s1.length(),n=s2.length();
 		//base condition
@@ -18,7 +26,8 @@ public:
         while(end<n){
             m2[s2[start++]-'a']--;
             m2[s2[end++]-'a']++;
-            if(m1==m2) return true;
+            if(areVectorEqual(m1,m2))
+                return true;
         }
         return false;
         
