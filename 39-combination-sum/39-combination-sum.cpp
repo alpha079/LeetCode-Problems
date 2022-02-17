ce4c549d8df08a2fb737ee0e61b6e9e3ee99a078
Shivam Soni <shivamkmv2311@gmail.com>
2022-02-17 08:17:43 +0530

    Time: 12 ms (56.55%), Space: 10.9 MB (58.65%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     
-    //TC:- O(2^N)
-    //TC Discussed here :- https://leetcode.com/problems/combination-sum/discuss/16634/If-asked-to-discuss-the-time-complexity-of-your-solution-what-would-you-say
-    void solve(vector<int>& num, int target,vector<vector<int>>& res,vector<int>& mid, int begin)
+    //Time:- O(2^N)
+    //[2,7,6,3,5,1] , 9
+    //Without sorting failing on this test case
+    void helper(vector<int>& num,int target,vector<vector<int>>& res,vector<int>& ans,int begin)
     {
         if(target==0)
         {
-            res.push_back(mid);
+            res.push_back(ans);
             return;
         }
-        for(int i=begin;i!=num.size() && target>=num[i];++i)
+        for(int i=begin;i<num.size()&& target>=num[i];i++)
         {
-            mid.push_back(num[i]);
-            solve(num,target-num[i],res,mid,i);
-            mid.pop_back();
+            ans.push_back(num[i]);
+            helper(num,target-num[i],res,ans,i);
+            ans.pop_back();
         }
-        
     }
-    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> ans;
         sort(candidates.begin(),candidates.end());
-         vector<vector<int>> res;
-        vector<int> mid;
-        solve(candidates,target,res,mid,0);
+        
+        helper(candidates,target,res,ans,0);
         return res;
         
     }
