29f7a2d2232dc0addb96c0c0efeb4288ddad0e80
Shivam Soni <shivamkmv2311@gmail.com>
2022-02-18 08:46:01 +0530

    Time: 7 ms (52.23%), Space: 7 MB (52.75%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -5,50 +5,28 @@ public:
         int n=num.size();
         if(k==n)return "0";
         
+        string res="";
         
-        stack<char> s;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<num.size();i++)
         {
-            if(s.empty())
+            
+            while(!res.empty() && res.back()>num[i] and k>0)
             {
-                s.push(num[i]);
+                res.pop_back();
+                k--;
             }
-            else{
-                
-                while(!s.empty() && s.top()>num[i] && k>0)
-                {
-                    k--;
-                    s.pop();
-                }
-                s.push(num[i]);
+            if(!res.empty() || num[i]!='0')
+            {
+                res+=num[i];
             }
         }
-    // Ex: "12345", k=3
-	// For the above example, while inserting the elements, no element will be popped
-	// But we need to remove 'k' characters 
-	// Hence remove the top k characters
-        while(k!=0)
+        //Now If case is "12345" k=3
+        while(k-- && !res.empty())
         {
-            s.pop();
-            k--;
+            res.pop_back();
         }
+        return res.size()>0 ? res: "0";
         
-    //Empty the stack to get the number;
-        string ans="";
-        while(!s.empty())
-        {
-            ans+=s.top();
-            s.pop();
-        }
-        
-        reverse(ans.begin(),ans.end());
-        cout<<ans;
-        
-        // Removing leading zeroes
-	while(ans.length() > 0 && ans[0] == '0'){
-		ans.erase(0, 1);
-	}
-        return ans.empty() ?"0" :ans;
         
     }
 };
