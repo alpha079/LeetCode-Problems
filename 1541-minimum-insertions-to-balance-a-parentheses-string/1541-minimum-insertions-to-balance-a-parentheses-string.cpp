b0d0ec714e5f34e8554ce81009d9ad4269e0a6ba
Shivam Soni <shivamkmv2311@gmail.com>
2022-02-22 12:46:03 +0530

    Time: 25 ms (96.86%), Space: 12.3 MB (90.71%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.cpp
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.cpp
@@ -1,50 +1,49 @@
 class Solution {
 public:
+ /*
+ //https://leetcode.com/problems/minimum-insertions-to-balance-a-parentheses-string/discuss/780199/JavaC%2B%2BPython-Straight-Forward-One-Pass
+ Example 1: Consider ((()(,n= 5 ,i=0,1...4
+i=0, we have ( it means we need two right parenthesis (they are in pair) so.. right+=2 => res =0, right =2
+i=1, again we have ( it means we need two right parenthesis (they are in pair) so.. right+=2 => res =0, right =4
+i=2, again we have ( it means we need two right parenthesis (they are in pair) so.. right+=2 => res =0, right =6
+i=3, we have ) we subtract one from right. so.. right-- => res =0, right =5
+i=4, we have ( but here right is odd so we need to make it even with right-- and increment res++ => res =1, right =4. Also, as we have got a left parenthesis then we need two right parenthesis (they are in pair) so.. right+=2 => res =1, right =6
+
+finally ans is res + right => 1 +6 == 7
+
+Ex:- ))
+ Right=0, right--, -1, right<0, right+=2, right=1, res=1;
+ Right--, right=0, right<0 , No
+ 
+ Right+ res= 0+1
+    
+*/
     int minInsertions(string s) {
         
-        stack<int> st;
-        int ans=0;
-        for(int i=0;i<size(s);i++)
+        int right=0,res=0;
+        
+        for(auto c: s)
         {
-            if(s[i]=='(')
+            if(c=='(')
             {
-                //if tsakc is empty or top os stack is 2, just again push 2;
-                if(st.empty() || st.top()==2)
+                if(right%2!=0)
                 {
-                    st.push(2);
+                    right--;
+                    res++;
                 }
-                else{
-                // if there is 1,that means one '(', pop that out, increase the count of ans, then again push 2
-                    st.pop();
-                    st.push(2);
-                    ans++;
+                right+=2;
                     
-                }
             }
             else{
-                
-            // Case :- if stack is empty, and we encountered ')', then push 1, and increment ans
-                if(st.empty())
+                right--;
+                if(right<0)
                 {
-                    st.push(1);ans++;
-                }
-            //case 2: if st.top is '1', just pop the element, st.pop(), we already increased the count in above 'if case'
-                else if(st.top()==1)
-                {
-                    st.pop();
-                }
-                //here st.top is '(', then pop first, then push 1.
-                else if(st.top()==2){
-                    st.pop();
-                    st.push(1);
+                    right+=2;
+                    res++;
                 }
             }
         }
-        while(!st.empty())
-        {
-            ans+=st.top();
-            st.pop();
-        }
-        return ans;
+        return res+ right;
+        
     }
 };
