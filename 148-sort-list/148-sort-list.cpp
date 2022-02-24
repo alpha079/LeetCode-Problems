c0ee30f51ae249cc3918408f8e176e793167d8eb
Shivam Soni <shivamkmv2311@gmail.com>
2022-02-24 14:35:52 +0530

    Time: 76 ms (99.22%), Space: 30.2 MB (66.97%) - LeetHub

---
+++ 148-sort-list/148-sort-list.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    ListNode* merge(ListNode* slow, ListNode* fast)
+    {
+        if(slow == NULL)
+            return fast;
+        if(fast == NULL)
+            return slow;
+        ListNode* f = NULL;
+        if(slow->val < fast->val)
+        {
+            f = slow;
+            f->next = merge(slow->next, fast);
+        }
+        else
+        {
+            f = fast;
+            f->next = merge(slow, fast->next);
+        }
+        return f;
+    }
+    ListNode* sort(ListNode* head)
+    {
+        if(head == NULL)
+            return NULL;
+        if(head->next == NULL)
+            return head;
+        ListNode* slow = head, *fast = head, *prev = head;
+        while(slow && fast)
+        {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next;
+            if(!fast)
+                break;
+            fast = fast->next;
+        }
+        prev->next = NULL;
+        head = sort(head);
+        slow = sort(slow);
+        return merge(head, slow);
+    }
+    ListNode* sortList(ListNode* head) {
+        return sort(head);
+    }
+};
