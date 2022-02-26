d4f4d31e9b8c7e0c10a90141dced78d68a456162
Shivam Soni <shivamkmv2311@gmail.com>
2022-02-26 13:55:00 +0530

    Time: 32 ms (68.12%), Space: 28.2 MB (90.07%) - LeetHub

--- 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.cpp
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.cpp
@@ -1,44 +1,33 @@
 
 
+//Find Mid of linked list and make mid as root.
+//Then recursively call like this
+//Just like the merge porcess of Merge Sort
 class Solution {
 public:
-    //Push the linked list into a vector
-    //Now construct the tree from that vector
-    
-    //Time:- O(N)
-    //SC:- O(N)
-    
-    TreeNode* constructTree(vector<int>& ar, int low, int high)
-    {
-        if(low>high)
-        {
-            return NULL;
-            
-        }
-        
-        int mid=(low+high)/2;
-        
-        TreeNode* root=new TreeNode(ar[mid]);
-        root->left=constructTree(ar,low,mid-1);
-        root->right=constructTree(ar,mid+1,high);
-        return root;
-    }
+    //TC:- O(nlogn)
+    //SC:- O(logn)
     TreeNode* sortedListToBST(ListNode* head) {
         
         if(head==NULL)
             return NULL;
         if(head->next==NULL)
             return new TreeNode(head->val);
-        vector<int> ans;
         
-        ListNode* node=head;
-        while(node)
+        ListNode* slow=head,*fast=head,*mid=NULL;
+        
+        while(fast and fast->next)
         {
-            ans.push_back(node->val);
-            node=node->next;
+            mid=slow;
+            slow=slow->next;
+            fast=fast->next->next;
         }
+        mid->next=NULL;
         
-        TreeNode* root= constructTree(ans,0,ans.size()-1);
+        TreeNode* root=new TreeNode(slow->val);
+        root->left=sortedListToBST(head);
+        root->right=sortedListToBST(slow->next);
+        
         return root;
     }
 };
