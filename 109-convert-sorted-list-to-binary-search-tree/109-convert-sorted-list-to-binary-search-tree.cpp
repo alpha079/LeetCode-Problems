9daede4b15a26ede625e9d1c2f6f8496382ce8a2
Shivam Soni <shivamkmv2311@gmail.com>
2022-02-26 15:05:59 +0530

    Time: 47 ms (34.40%), Space: 28.4 MB (36.56%) - LeetHub

--- 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.cpp
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.cpp
@@ -1,32 +1,33 @@
-
-
-//Find Mid of linked list and make mid as root.
-//Then recursively call like this
-//Just like the merge porcess of Merge Sort
 class Solution {
 public:
-    //TC:- O(nlogn)
-    //SC:- O(logn)
     TreeNode* sortedListToBST(ListNode* head) {
+        //M3: traverse LL, while simulate in-order traverse sequence
+        int right = 0;
+        ListNode* tmp = head;
         
-        if(head==NULL)
-            return NULL;
-        if(head->next==NULL)
-            return new TreeNode(head->val);
-        
-        ListNode* slow=head,*fast=head,*mid=NULL;
-        
-        while(fast and fast->next)
-        {
-            mid=slow;
-            slow=slow->next;
-            fast=fast->next->next;
+        while(tmp) {
+            tmp = tmp->next;
+            ++right;
         }
-        mid->next=NULL;
         
-        TreeNode* root=new TreeNode(slow->val);
-        root->left=sortedListToBST(head);
-        root->right=sortedListToBST(slow->next);
+        return build(0, right - 1, head);
+    }
+    
+private:
+    TreeNode* build(int L, int R, ListNode* &head) {
+        if (L > R) return nullptr;
+        
+        int mid = L + (R - L) / 2;
+        
+        TreeNode* left = build(L, mid - 1, head);
+        
+		//head in LL is root in In-order traverse, after finishing left side
+        TreeNode* root = new TreeNode (head->val);
+        head = head->next;  //move to next node in LL
+        
+		//connect tree
+		root->left = left;
+        root->right = build(mid + 1, R, head);
         
         return root;
     }
