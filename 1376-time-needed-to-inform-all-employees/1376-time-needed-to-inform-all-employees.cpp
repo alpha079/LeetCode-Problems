0d6fa02ec5269b408c50108845830055a72ea8d4
Shivam Soni <shivamkmv2311@gmail.com>
2022-02-27 16:12:17 +0530

    Time: 365 ms (68.01%), Space: 126.9 MB (40.64%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
+    //Post LInk:
+    //https://leetcode.com/problems/time-needed-to-inform-all-employees/discuss/755436/C%2B%2B-BFS-Solution-Explained-~80-Time-~50-Space
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         
         
+        //Time:- O(N)
         vector<vector<int>> graph(n);
         
         for(int i=0;i<n;i++)
