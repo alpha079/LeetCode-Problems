bb37da51a5b3748b730800b0a119a13bb7cf0071
Shivam Soni <shivamkmv2311@gmail.com>
2022-03-01 09:17:38 +0530

    Time: 8 ms (52.50%), Space: 8.6 MB (21.58%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
+    
+    //TIme:- O(Nlogn)
     vector<int> countBits(int num) {
-        vector<int> res(num+1,0);
+        vector<int> res;
+        res.push_back(0);
+        if(num==0)
+            return res;
         
-        for(int i=0;i<=num;i++)
+        //for each number, count the set bits;
+        
+        for(int i=1;i<=num;i++)
         {
-            res[i]=res[i/2]+ i%2;
-            
+            int sum=0;
+            int number=i;
+            while(number!=0)
+            {
+                sum+=number%2;
+                number/=2;
+            }
+            res.push_back(sum);
         }
         return res;
-        
     }
 };
