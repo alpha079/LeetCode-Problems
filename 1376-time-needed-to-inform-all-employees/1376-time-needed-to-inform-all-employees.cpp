3f3e90cf22ee274204693333a52ab57c4369da59
Shivam Soni <shivamkmv2311@gmail.com>
2022-03-03 09:48:28 +0530

    Time: 1784 ms (5.00%), Space: 90.8 MB (96.30%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,47 +1,23 @@
 class Solution {
 public:
-    //Post LInk:
-    //https://leetcode.com/problems/time-needed-to-inform-all-employees/discuss/755436/C%2B%2B-BFS-Solution-Explained-~80-Time-~50-Space
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         
+        //TC:- O(N^2)
+        // We can optimise it by declaring a visiting array, that the Employee has
+        // been already informed
+        int ans=0;
         
-        //Time:- O(N)
-        vector<vector<int>> graph(n);
-        
-        for(int i=0;i<n;i++)
+        for(int i=0;i<manager.size();i++)
         {
-            if(i!=headID)
+            int j=i;
+            int time=0;
+            while(j!=headID)
             {
-                graph[manager[i]].push_back(i);
+                time+=informTime[manager[j]];
+                j=manager[j];
             }
+            ans=max(time,ans);
         }
-        queue<pair<int,int>> q;
-        q.push({0,headID});
-        int res=0;
-        while(!q.empty())
-        {
-            int size=q.size();
-            
-            while(size--)
-            {
-                auto time=q.front().first;
-                auto node=q.front().second;
-                q.pop();
-                
-                if(graph[node].size())
-                {
-                    for(auto it: graph[node])
-                    {
-                        q.push({time+informTime[node],it});
-                    }
-                }
-                else{
-                    res=max(res,time);
-                }
-                
-            }
-        }
-        return res;
-        
+        return ans;
     }
 };
