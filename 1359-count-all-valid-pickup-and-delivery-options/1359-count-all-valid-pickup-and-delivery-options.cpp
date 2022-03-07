be9231a6955e055fd569eda9aeb2fd3afca4ad21
Shivam Soni <shivamkmv2311@gmail.com>
2022-03-07 10:16:39 +0530

    Time: 4 ms (43.63%), Space: 5.9 MB (82.17%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,27 +1,19 @@
-//https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/discuss/1824108/Recursion-with-memo-(with-explanation-)
+//https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/discuss/516940/Simple-Java-DP
 
 class Solution {
 public:
-    int mod = pow(10,9)+7;
-    
-    long long totalWays(int pickup, int deliver, vector<vector<long>>&dp)
-    {
-      
-        if(deliver<pickup || pickup<0 || deliver<0)
-        {
-            return 0;
-        }
-        if(dp[pickup][deliver]==0)
-        {
-             dp[pickup][deliver]=pickup*totalWays(pickup-1,deliver,dp)%mod+(deliver-pickup)*totalWays(pickup,deliver-1,dp)%mod;
-        }
-       
-        
-        return dp[pickup][deliver]%mod;
-    }
+    //TC:- O(N)
     int countOrders(int n) {
-        vector<vector<long>>dp(n+1,vector<long>(n+1,0));
-        dp[0][0] = 1;
-        return totalWays(n,n,dp);
+        long dp[501]={0};
+        dp[1]=1L;
+        dp[2]=6L;
+        int mod = pow(10,9)+7;
+        for(int i=3;i<=n;i++)
+        {
+            int spaceCount=2*(i-1)+1;
+            long val=(spaceCount*(spaceCount+1)/2)%mod;
+            dp[i]=(dp[i-1]*val)%mod;
+        }
+        return dp[n]%mod;
     }
 };
