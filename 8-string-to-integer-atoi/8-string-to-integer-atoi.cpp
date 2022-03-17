ba8d1c5ef0b9b2ebec84a501689e9b2fc455f442
Shivam Soni <shivamkmv2311@gmail.com>
2022-03-17 09:00:15 +0530

    Time: 3 ms (77.18%), Space: 7.1 MB (53.78%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -14,13 +14,14 @@ public:
         
         // read the next char's until and unless the non digit char occurs or end of the string reached
         // if non digit char occurs just ignore the rest of the string
+        const int overflow = INT_MAX / 10;
         int num = 0;
         while(itr < n && s[itr] >= '0' && s[itr] <= '9'){
             
             // if(num > INT_MAX/10) is true, then you should return INT_MAX, but 
             // if they are equal(num == INT_MAX/10), then check the unit place(s[itr] - '0' > 7), now why 7? bcz
             // INT_MAX means this value (2147483647), its having 7 on its unit place, so suppose (2147483647/10) is equal to x(any number) value, then we need to check the unit place, right, if its more than 7, it means that is big and our condition will become true, otherwise if its small then condition will become false
-            if(num > INT_MAX/10 || (num == INT_MAX/10 && s[itr] - '0' > 7)){
+            if(num >overflow || (num == overflow && s[itr] - '0' > 7)){
                 return (sign == 1) ? INT_MAX : INT_MIN;
             }
             
