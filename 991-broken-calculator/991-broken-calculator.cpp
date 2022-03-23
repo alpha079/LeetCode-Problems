29b27211a12b8c3281db604e5994c4421d5de680
Shivam Soni <shivamkmv2311@gmail.com>
2022-03-23 20:19:25 +0530

    Time: 0 ms (100.00%), Space: 6 MB (30.21%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-
     int brokenCalc(int startValue, int target) {
-        //ALGO: Greedy approach, we move reverse from target to startValue.
-        //So applying reverse logic, at every step we can:
-        //(i) divide target / 2 if target is even
-        //(ii) add target by 1 if target is odd, in order to make it even
-        int targ = target;
-        int steps = 0;
-        while(targ>startValue)
-        {
-            if((targ&1)==1)
-                ++targ;
-            else
-                targ/=2;
-            steps++;
+        long long ans = 0;
+        long long tar = target;
+        while(startValue != tar){
+            if(tar%2 == 0 and startValue < tar){
+                tar /= 2;
+            }
+            else if(tar > startValue){
+                tar +=1;
+            }
+            else{
+                ans += abs(tar-startValue)-1;
+                tar = startValue;
+            }
+            ans++;
         }
-        return steps+(startValue-targ); //adding startValue-target, to handle the case if target stops at a value less than startValue, then we have to add 1 that many times
+        return ans;
     }
 };
