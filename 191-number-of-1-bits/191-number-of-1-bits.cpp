ecb15739818aa96b14a56f3fe31c7cc9ac1ea8dc
Shivam Soni <shivamkmv2311@gmail.com>
2022-05-26 09:28:56 +0530

    Time: 3 ms (54.92%), Space: 6 MB (48.74%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -3,11 +3,19 @@ public:
     int hammingWeight(uint32_t n) {
         
         int cnt=0;
-        
-        while(n>0)
+      /*  Here is the thing,
+finding n&(n-1) is a way to remove the least significant bit (LSB).
+
+Here is an example (n=1011) :
+n=1011 (n-1)=1010 n&(n-1)=1011 & 1010 = 1010 (new n) -- count=1
+n=1010 (n-1)=1001 n&(n-1)=1010 & 1001 = 1000 (new n) -- count=2
+n=1000 (n-1)=0111 n&(n-1)=1000 & 0111 = 0000 (condition to exit while) -- count=3
+
+*/
+        while(n)
         {
-            cnt+=n&1;
-            n=n>>1;
+          n=n&(n-1);
+            cnt++;
         }
         return cnt;
         
