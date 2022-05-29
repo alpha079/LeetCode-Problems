bbe25a81db20560b9d5a5e30c95206810f778b46
Shivam Soni <shivamkmv2311@gmail.com>
2022-05-29 13:24:35 +0530

    Time: 27 ms (35.94%), Space: 16.9 MB (92.74%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
+    //Time: O(Nlogn)
+    //SC:- O(res), equal to the merged intervals.
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         
-        intervals.push_back(newInterval);
         
-        sort(intervals.begin(),intervals.end());
         
-        int res=0;
+       vector<vector<int>> ans;
         
-        vector<vector<int>> ans;
-        //auto it= intervals[0];
-        for(int i=1;i<intervals.size();i++)
+        //1: First skip the intervals that are not within the newIntervals
+        //2: Merge the intervals till the newIntervals Range
+        //3: Append the larger intervals then the new Intervals
+        
+        //Given: The List is already sorted
+        int high=intervals.size();
+        int low=0;
+        
+        //1: 
+        while(low<high && intervals[low][1]<newInterval[0])
         {
-            if(intervals[i][0]<=intervals[res][1])
-            {
-                intervals[res][0]=min(intervals[i][0],intervals[res][0]);
-                intervals[res][1]=max(intervals[i][1],intervals[res][1]);
-            }
-            else
-            {
-                res++;
-                intervals[res]=intervals[i];
-            }
-            
+            ans.push_back(intervals[low]);
+            low++;
         }
-        for(int i=0;i<=res;i++)
+        
+        //2:
+        while(low<high && newInterval[1]>=intervals[low][0])
         {
-            ans.push_back(intervals[i]);
+            newInterval[0]=min(newInterval[0],intervals[low][0]);
+            newInterval[1]=max(newInterval[1],intervals[low][1]);
+            
+            low++;
+        }
+        ans.push_back(newInterval);
+        
+        while(low<high)
+        {
+            ans.push_back(intervals[low]);
+            low++;
         }
         return ans;
     }
