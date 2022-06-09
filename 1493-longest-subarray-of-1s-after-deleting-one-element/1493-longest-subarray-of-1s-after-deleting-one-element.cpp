a7d0cb01a483f447f8606e1fb390259b42af062b
Shivam Soni <shivamkmv2311@gmail.com>
2022-06-09 08:12:31 +0530

    Time: 48 ms (70.20%), Space: 36.6 MB (18.67%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int skip)
-{
-	int cnt=0;
-	int len=0;
-	for(int i=0;i<nums.size();i++)
-	{
-		if(i==skip)continue;
-		if(nums[i]==1)
-		{
-			cnt++;
-			len=max(len,cnt);
 
-		}
-		else cnt=0;
-	}
-	return len;
-}
     int longestSubarray(vector<int>& nums) {
         if(nums.size()==0)return 0;
 
-	int zeroes=0;
+        vector<int> dp;
+        int cnt=0;
+        nums.push_back(0);
+        for(auto it: nums)
+        {
+            if(it==1)
+            {
+                cnt++;
+            }
+            else{
+                dp.push_back(cnt);
+                cnt=0;
+            }
+        }
+        if(dp.size()==1)
+            return dp[0]-1;
         int ans=0;
-
-	for(int i=0;i<nums.size();i++)
-	{
-		if(nums[i]==0)
-		{
-			//start counting the number of 1's excluding the current 0;
-			ans=max(ans,helper(nums,i));
-			zeroes++;
-		}
-	}
-	if(zeroes==0)
-		return nums.size()-1;
-	return ans;
+        for(int i=0;i<dp.size()-1;i++)
+        {
+            ans=max(ans,dp[i]+dp[i+1]);
+        }
+        return ans;
     }
 };
