32f451cb5fabb715ae65c0dc55f62a0234e96bbb
Shivam Soni <shivamkmv2311@gmail.com>
2022-06-10 12:13:18 +0530

    Time: 83 ms (37.08%), Space: 34.8 MB (36.35%) - LeetHub

---
+++ 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int k) {
+         int prefixSum=0;
+        
+        unordered_map<int,int> map;
+        map[prefixSum]=1;   //[0,1] This is important
+        int res=0;
+        for(auto it: nums)
+        {
+            prefixSum+=it;
+            
+            if(map.find(prefixSum-k)!=map.end())
+            {
+                res+=map[prefixSum-k];
+            }
+            
+            map[prefixSum]++; //This Step will handle below both steps
+            /*
+            if(mp.find(prefixSum)!=mp.end())
+				mp[prefixSum] = mp[prefixSum] + 1;
+
+			//is prefixSum not present in hashMap then give count = 1
+			else
+				mp[prefixSum] = 1;
+                */
+        }
+        return res;
+    }
+};
