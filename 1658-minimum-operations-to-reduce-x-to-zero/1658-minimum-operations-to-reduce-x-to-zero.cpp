f36cc3f97f9fd400bb4b9fe86adcb01d10a3d76f
Shivam Soni <shivamkmv2311@gmail.com>
2022-06-12 15:11:13 +0530

    Time: 288 ms (56.95%), Space: 98.6 MB (71.22%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,36 +2,28 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         
-        unordered_map<int,int> mp;
-        int ans=INT_MAX;
-                
-        int n=nums.size();
-
-        for(int i=0,preSum=0;i<nums.size();i++)
-        {
-            preSum+=nums[i];
-            mp[preSum]=i+1;
-            if(preSum==x)
-            {
-                ans=min(ans,i+1);
-            }
-        }
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        if(sum < x) return -1;
+        if(sum==x)return nums.size();
+        int target=sum-x;
+        int low=0;
         
-        for(int i=nums.size()-1,sufSum=0;i>=0;i--)
+        int ans=0;
+        int current=0;
+        for(int high=0;high<nums.size();high++)
         {
-            sufSum+=nums[i];
-            if(sufSum==x)
-            {
-                
-                ans=min(ans,n-i);
-            }
-            if(mp[x-sufSum]>0 && i+1>mp[x-sufSum])
-            {
-                ans=min(ans,n-i+mp[x-sufSum]);
-            }
+            current+=nums[high];
             
+            while(current>target)
+            {
+                current-=nums[low++];
+            }
+            if(current==target)
+            {
+                ans=max(ans,high-low+1);
+            }
+         
         }
-        return ans==INT_MAX?-1: ans;
-        
+        return ans==0 ?-1:nums.size()-ans;
     }
 };
