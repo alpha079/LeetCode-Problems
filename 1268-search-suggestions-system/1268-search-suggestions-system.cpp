e14ba2f44654db9d4c96f3bdf820efe82c592981
Shivam Soni <shivamkmv2311@gmail.com>
2022-06-20 08:08:49 +0530

    Time: 52 ms (90.46%), Space: 26.2 MB (87.86%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,45 +1,28 @@
+//Time Complexity: O(NlogN) + O(MlogN), where N=|products|, M= |searchWord|
+//Space: O(1)
 class Solution {
 public:
-    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+    vector<vector<string>> suggestedProducts(vector<string>& prod, string s) {
         
         
-        vector<vector<string>>ans;
-        int i,j;
-        int n=searchWord.size();
-        sort(products.begin(),products.end());
-        for(i=0;i<n;i++)
+        sort(prod.begin(),prod.end());
+        vector<vector<string>> ans;
+        int start(0), bStart(0),n=prod.size();
+        
+        string prefix="";
+        for(char c: s)
         {
-            string cw=searchWord.substr(0,i+1);
+            vector<string> res;
+            prefix+=c;
+            start=lower_bound(prod.begin()+bStart,prod.end(),prefix)-prod.begin();
             
-            vector<string>res;
-            int low=0;
-            int high=products.size();
-            
-            while(low<high)
+            for(int i=start;i<min(start+3,n)&& !prod[i].compare(0,prefix.length(),prefix);i++)
             {
-                int mid = low + (high - low)/2;
-                cout<<products[mid]<<endl;
-                if(products[mid]>=cw)
-                {
-                    high=mid;
-                }
-                else
-                {
-
-                    low=mid+1;
-                }
+                
+                res.push_back(prod[i]);
             }
-            cout<<low<<endl;
-            for(int j=low;j<low+3 and j<products.size();j++)
-            {
-             if(products[j].find(cw) == 0){
-                    res.push_back(products[j]);
-                }else{
-                    break;
-                }
-            }
-            
             ans.push_back(res);
+            bStart=start;
         }
         return ans;
     }
