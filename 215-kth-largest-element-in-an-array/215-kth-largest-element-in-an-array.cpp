09f376d1beb3359dc61727869b77d56956a5e22b
Shivam Soni <shivamkmv2311@gmail.com>
2022-06-23 11:22:15 +0530

    Time: 130 ms (8.68%), Space: 9.9 MB (86.13%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,20 +1,58 @@
-//https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/60309/C%2B%2B-STL-partition-and-heapsort
 class Solution {
 public:
+    
+    void swap(vector<int>& nums, int i, int j)
+{
+	int temp = nums[i];
+	nums[i] = nums[j];
+	nums[j] = temp;
+}
+    int findCorrectPivPosition(vector<int>& nums, int low,int r)
+    {
+        int piv=r;
+        r--;
+        while(low<=r)
+        {
+            
+            while(low<=r && nums[low]<=nums[piv])
+            {
+                low++;
+            }
+            while(low<=r && nums[r]>=nums[piv])
+            {
+                r--;
+            }
+            if(r >low)
+            {
+                //test case:- [3,1,2,4]
+                // 2
+                swap(nums,low,r);
+            }
+            
+        }
+        swap(nums,low,piv);
+        return low;
+    }
     int findKthLargest(vector<int>& nums, int k) {
         
-        priority_queue<int,vector<int>,greater<int>>pq(nums.begin(),nums.begin()+k); //(klogk)
-        
-        for(int i=k;i<nums.size();i++) //O(n)
+        int left=0;
+        int right=nums.size()-1;
+        k=nums.size()-k;
+        while(left<=right)
         {
-            if(nums[i]>pq.top())
+            int pos=findCorrectPivPosition(nums,left,right);
+            if(pos<k)
             {
-                pq.pop();
-                pq.push(nums[i]);
+                left=pos+1;
             }
+            else if(pos>k)
+            {
+                right=pos-1;
+            }
+            else break;
+            
+            
         }
-        return pq.top();
+        return nums[k];
     }
-    
-    //O(n +klogk)
 };
