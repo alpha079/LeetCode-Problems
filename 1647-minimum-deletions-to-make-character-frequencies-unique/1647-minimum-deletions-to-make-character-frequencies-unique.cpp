2ce5026bf8a9eb9018cdb8303972c11551890fc5
Shivam Soni <shivamkmv2311@gmail.com>
2022-06-28 08:05:16 +0530

    Time: 184 ms (15.10%), Space: 17.3 MB (58.78%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        
+     unordered_map<char,int> map;
+        
+        for(auto it: s)
+        {
+            map[it]++;
+        }
+        unordered_set<int> set;
+        int ans=0;
+        
+        for(auto [key,value]: map)
+        {
+            while(set.find(value)!=set.end())
+            {
+                value--;
+                ans++;
+            }
+            if(value>0)
+                set.insert(value);
+        }
+        return ans;
+        
+    }
+};
