e8634d42f98273c5b26101359b2846a0041a4e5e
Shivam Soni <shivamkmv2311@gmail.com>
2022-06-28 13:10:28 +0530

    Time: 392 ms (22.04%), Space: 121.6 MB (7.81%) - LeetHub

--- 945-minimum-increment-to-make-array-unique/945-minimum-increment-to-make-array-unique.cpp
+++ 945-minimum-increment-to-make-array-unique/945-minimum-increment-to-make-array-unique.cpp
@@ -1,20 +1,22 @@
-
-//TC: O(Nlogn)
 class Solution {
 public:
+    const int N = 2e5 + 5;
     int minIncrementForUnique(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-       int ans=0;
         
-        for(int i=1;i<nums.size();i++)
+        vector<int> freq(N,0);
+        
+        for(auto it: nums)
         {
-            if(nums[i]<=nums[i-1])
-            {
-                ans+=(nums[i-1]+1-nums[i]);
-                nums[i]=nums[i-1]+1;
-            }
+            freq[it]++;
+        }
+        int ans=0;
+        for(int i=0;i<N;i++)
+        {
+            if(freq[i]<=1)continue;
+            
+            freq[i+1]+=freq[i]-1;
+            ans+=freq[i]-1;
         }
         return ans;
-        
     }
 };
