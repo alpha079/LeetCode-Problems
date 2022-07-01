3675b34d0eea8050b6732a1a2cf02dac54c2b72d
Shivam Soni <shivamkmv2311@gmail.com>
2022-07-01 08:49:21 +0530

    Time: 46 ms (89.84%), Space: 17.8 MB (30.21%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    //TC:- O(NlognN)
+    //TC:- O(N) Counting Sort
     int maximumUnits(vector<vector<int>>& boxes, int truckSize) {
         
-       sort(begin(boxes), end(boxes), [](auto &a, auto &b){return a[1] > b[1];});
-        
-        int ans=0;
-        
+       vector<int> freq(1001,0);
         for(auto it: boxes)
         {
-            if(truckSize<=0)break;
-            ans+=min(truckSize,it[0])*it[1];
-            truckSize-=it[0];
+            freq[it[1]]+=it[0];
         }
-        return ans;
+        int res=0;
         
-    }
-    static bool cmp(vector<vector<int>>& a, vector<vector<int>>& b)
-    {
-        return a[1]>b[1];
+        for(int i=1000;i>=0 and truckSize>0;i--)
+        {
+            res+=min(truckSize,freq[i])*i;
+            truckSize-=freq[i];
+        }
+        return res;
     }
 };
