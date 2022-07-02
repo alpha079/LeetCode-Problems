56a1dea65eea993fd2f9ff9fea265a8fc66ddf96
Shivam Soni <shivamkmv2311@gmail.com>
2022-07-02 10:09:54 +0530

    Time: 55 ms (40.58%), Space: 14 MB (82.93%) - LeetHub

--- 690-employee-importance/690-employee-importance.cpp
+++ 690-employee-importance/690-employee-importance.cpp
@@ -12,29 +12,27 @@ class Solution {
 public:
     //using Recursion
     //Tc:- O(N)
+    
+    unordered_map<int,Employee*> map;
+    int dfs(int id)
+    {
+        Employee* emp=map[id];
+         int result=emp->importance;
+        for(auto it: emp->subordinates)
+        {
+            result+=dfs(it);
+        }
+        return result;
+    }
     int getImportance(vector<Employee*> employees, int id) {
         int result=0;
         
-        for(auto emp: employees)
-        {
-            if(emp->id==id)
-            {
-                result+=emp->importance;
-                
-                if(!emp->subordinates.empty())
-                {
-                    for(auto it: emp->subordinates)
-                  {
-                    result+=getImportance(employees,it);
-                  }
-                    
-                }
-                return result;
-                
-            }
-            
-        }
-        return result;
+         for(auto it: employees)
+         {
+             map[it->id]=it;
+         }
+        return dfs(id);
+        
         
     }
 };
