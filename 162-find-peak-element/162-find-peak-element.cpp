da180f6dfdbf23ab9b08857756df2941cfbc4005
Shivam Soni <shivamkmv2311@gmail.com>
2022-07-03 12:21:09 +0530

    Time: 3 ms (91.04%), Space: 8.8 MB (89.20%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -2,23 +2,48 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         
-        if(nums.size()==1)
-            return 0;
-        if(nums[0]>nums[1])
-            return 0;
-        if(nums[nums.size()-1]>nums[nums.size()-2])
-            return nums.size()-1;
+       //Apply Binary Search/
+       // Three cases
+        // 1: When mid is at zero index
+        // 2: When mid is at nums.size()-1
+        // 3: When mid is lying between 0 and nums.size()-1
         
-        int peak=-1;
-        for(int i=1;i<nums.size()-1;i++)
+        int low=0,high=nums.size()-1;
+        if(low==high)return 0; //only one element is there
+        
+        while(low<=high)
         {
-            if(nums[i]>nums[i-1] and nums[i]>nums[i+1])
+            int mid= low+ (high-low)/2;
+            cout<<mid<<endl;
+            
+            if(mid>0 and mid<nums.size()-1)
             {
-                peak=i;
+                if(nums[mid]>nums[mid+1] and nums[mid]>nums[mid-1])
+                {
+                    return mid;
+                }
+                else if(nums[mid]<nums[mid+1])
+                {
+                    low=mid+1;
+                }
+                else high=mid;
             }
-            else continue;
+            else if(mid==0)  //extreme left condition
+            {
+                if(nums[mid]>nums[mid+1])
+                    return mid;
+                else return mid+1;
+            }
+            else if(mid==nums.size()-1) //extreme right condition 
+			{
+                cout<<"3"<<endl;
+				if(nums[mid]>nums[mid-1])
+					return mid;
+				else
+					return mid-1;
+			}
         }
-        return peak;
+        return -1;
         
     }
 };
