4ad6c50e33d6ea6d89c9f5f916f14abb43a92306
Shivam Soni <shivamkmv2311@gmail.com>
2022-07-07 09:22:21 +0530

    Time: 22 ms (17.31%), Space: 10.2 MB (28.65%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    int helper(vector<vector<int>>& grid, int row, int col,vector<vector<int>>& dp)
+ /*   int helper(vector<vector<int>>& grid, int row, int col,vector<vector<int>>& dp)
     {
         if(row==0 and col==0)
             return grid[row][col];
@@ -13,10 +13,25 @@ public:
         int right=helper(grid,row,col-1,dp);
         return dp[row][col]=min(left,right)+ grid[row][col];
     }
+    */
     int minPathSum(vector<vector<int>>& grid) {
+         const int INF= 1e9 + 5;
          int row = grid.size(), col = grid[0].size();
         vector<vector<int>>dp(row,vector<int>(col,-1));
-        return helper(grid,row-1,col-1,dp);
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(i==0 and j==0)
+                    dp[i][j]=grid[i][j];
+                else{
+                    dp[i][j]=grid[i][j] + min((i==0?INF:dp[i-1][j]),(j==0?INF:dp[i][j-1]));
+                }
+            }
+        }
+        return dp[row-1][col-1];
+        //TC:- O(M*N)
+        //SC: O(M*N)
         
         
     }
