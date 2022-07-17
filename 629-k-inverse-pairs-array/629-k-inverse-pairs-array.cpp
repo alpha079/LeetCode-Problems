7d03f7b60b6b66439cfb7b1743ff125e8bac8956
Shivam Soni <shivamkmv2311@gmail.com>
2022-07-17 22:29:48 +0530

    Time: 8 ms (98.72%), Space: 5.9 MB (99.36%) - LeetHub

---
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/k-inverse-pairs-array/discuss/846076/C%2B%2B-4-solutions-with-picture
+class Solution {
+public:
+    int kInversePairs(int n, int k) {
+    int dp[2][1001] = { 1 };
+    for (int N = 1; N <= n; ++N)
+        for (int K = 0; K <= k; ++K) {
+            dp[N % 2][K] = (dp[(N - 1) % 2][K] + (K > 0 ? dp[N % 2][K - 1] : 0)) % 1000000007;
+            if (K >= N)
+                dp[N % 2][K] = (1000000007 + dp[N % 2][K] - dp[(N - 1) % 2][K - N]) % 1000000007;
+        }
+    return dp[n % 2][k];
+}
+};
