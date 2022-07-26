cdcdf15c399ee3f4cee30553a5e3e01d167217cb
Shivam Soni <shivamkmv2311@gmail.com>
2022-07-26 13:21:49 +0530

    Time: 24 ms (54.17%), Space: 14.2 MB (57.81%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,52 +9,23 @@
  */
 class Solution {
 public:
-    //Push the path of first node in one vector
-    //Push the path of second noe in second vector
-    
-    bool find(TreeNode* root,vector<TreeNode*>& v1,TreeNode* p)
-    {
-        if(root==NULL)
-            return false;
-        v1.push_back(root);
-        if(root==p)
-            return true;
-        if(find(root->left,v1,p) || find(root->right,v1,p))
-            return true;
-        v1.pop_back();
-        return false;
-    }
-    
-    TreeNode* matchingPoint(vector<TreeNode*>& v1,vector<TreeNode*>& v2)
-    {
-        
-        TreeNode* res=NULL;
-        
-        int n=v1.size();
-        int n1=v2.size();
-        for(int i=0;i<n &&i< n1;i++)
-        {
-            if(v1[i]!=v2[i])
-                return res;
-            
-            res=v1[i];
-        }
-        return res;
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
-       if(!root|| root==p || root==q)
-           return root;
+        if(root==NULL)
+            return NULL;
+        if(root==p || root==q)
+            return root;
+        TreeNode* leftNode=lowestCommonAncestor(root->left,p,q);
+        TreeNode* rightNode=lowestCommonAncestor(root->right,p,q);
         
-        vector<TreeNode*> v1;
-        vector<TreeNode*> v2;
+        if(leftNode!=NULL and rightNode!=NULL)
+            return root;
+        if(leftNode!=NULL)
+            return leftNode;
+        if(rightNode!=NULL)
+            return rightNode;
         
-        bool x=find(root,v1,p);
-        bool y=find(root,v2,q);
-        
-        return matchingPoint(v1,v2);
-        
-        
+        return NULL;
         
     }
 };
