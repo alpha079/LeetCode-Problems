a4bc8243b6e218ca597a487daf0e347c2ae7e163
Shivam Soni <shivamkmv2311@gmail.com>
2022-08-30 21:53:08 +0530

    Time: 3 ms (64.32%), Space: 7.2 MB (33.50%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int layer = 0; layer < n/2; layer++){
+            for (int i = layer; i < n-1-layer; i++){
+                int tmp = matrix[layer][i];
+                matrix[layer][i] = matrix[n-1-i][layer];
+                matrix[n-1-i][layer] = matrix[n-1-layer][n-1-i];
+                matrix[n-1-layer][n-1-i] = matrix[i][n-1-layer];
+                matrix[i][n-1-layer] = tmp;
+            }
+        }
+        return;
+    }
+};
