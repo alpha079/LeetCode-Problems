10401e5bbdce1a0bf96a8a4b261f2f6e013eaaf2
Shivam Soni <shivamkmv2311@gmail.com>
2022-09-14 16:05:41 +0530

    Time: 19 ms (71.34%), Space: 10.4 MB (67.09%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     
+    int search(int key, vector<int>& res)
+    {
+        int find=0;
+        int low=0,high=res.size()-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            
+            if(res[mid]>=key)
+            {
+                find=mid;
+                high=mid-1;
+            }
+            else{
+             low=mid+1;
+            }
+     
+        }
+        return find;
+    }
     //DP solution
     int lengthOfLIS(vector<int>& nums) {
         
@@ -14,17 +34,22 @@ public:
             {
                 res.push_back(nums[i]);
             }
-            else{
+           /* else{
                 int x=lower_bound(res.begin(),res.end(),nums[i])-res.begin();
                cout<<x<<endl;
                 // We are replacing the element at position, not inserting. 
                 // If element is already present at that position , it will be replaced
                 res[x]=nums[i];
+            }*/
+            else{
+                //Think like res array is sorted array, just do Binary Search
+                int x=search(nums[i],res);
+                res[x]=nums[i];
             }
         }
         
                return res.size();
-        
+        //TC:- O(nlogn);
     }
 
 };
