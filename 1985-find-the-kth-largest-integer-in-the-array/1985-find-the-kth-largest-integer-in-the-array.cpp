a3f30f2abfc10a1ad5e7676ecd67bf7f009fd1ac
Shivam Soni <shivamkmv2311@gmail.com>
2022-09-14 12:26:06 +0530

    Time: 327 ms (72.41%), Space: 69.6 MB (46.49%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -8,12 +8,37 @@ public:
             return a>b;
         else return a.size()>b.size();
     }
+    struct comparator
+    {
+        bool operator () (string& a, string& b)
+        {
+            if(a.size() == b.size())
+                return a > b;
+            else
+                return a.size() > b.size();
+        }
+    };
+    
     string kthLargestNumber(vector<string>& nums, int k) {
         
         
-        sort(nums.begin(),nums.end(),compare);
+        //sort(nums.begin(),nums.end(),compare);
         
-        return nums[k-1];
+        priority_queue<string,vector<string>,comparator> pq;//Min-Heap declaration
+        //TC:- O(nlogk)
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i<k)
+            {
+                pq.push(nums[i]);
+            }
+            else{
+                pq.push(nums[i]);
+                pq.pop();
+                
+            }
+        }
+        return pq.top();
         
     }
 };
