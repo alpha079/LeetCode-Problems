910616fefa75b4e56bc3f671997438ea11e06d1b
Shivam Soni <shivamkmv2311@gmail.com>
2022-09-18 17:36:58 +0530

    Time: 353 ms (9.65%), Space: 7.3 MB (96.98%) - LeetHub

---
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    //PARAMETERS:
+    //i & j : Ends of the array.
+    //turn : 1 If turn of player A. 0 If turn of player B.
+    int find(int i, int j, bool turn, vector<int> &array){
+        if(i>j) return 0;
+
+        //if(dp[i][j][turn]!=-1) return dp[i][j][turn]; 
+
+        //PLAYER A: Maximize the score of player A.
+        if(turn){
+            int pathA = array[i] + find(i+1, j, 0, array);
+            int pathB = array[j] + find(i, j-1, 0, array);
+            return  max(pathA, pathB);
+        }
+
+        //PLAYER B: Minimize the score of player A.
+        else{
+            int pathA = find(i+1, j, 1, array);
+            int pathB = find(i, j-1, 1, array);
+            return min(pathA, pathB);
+        }
+        
+    }
+    
+    bool PredictTheWinner(vector<int>& array) {
+        int i=0; int j=array.size()-1;
+        int n = array.size();
+       // vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(2, -1)));
+        int pA = find(i, j, 1, array);
+
+        int total=0; for(const int &i: array) total+=i;
+        int pB = total - pA;
+
+        return (pA>=pB);
+    }
+};
