249030973cc2d9728f34d09ef08fe89d1a2218be
Shivam Soni <shivamkmv2311@gmail.com>
2022-09-25 13:22:02 +0530

    Time: 294 ms (89.12%), Space: 11 MB (99.96%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-   vector<vector<int>>dp;
- //https://leetcode.com/problems/maximum-length-of-repeated-subarray/discuss/1324737/Optimization-from-Brute-Force-to-DP-to-Space-Optimized-DP-Explained! 
-    
+    // DP Approach - Space Optimized
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-        
-        int m=nums1.size();
-        int n=nums2.size();
-        
-        int dp[m+1][n+1];
-        memset(dp,0,sizeof(dp));
-        int ans=0;
-        for(int i=1;i<=m;i++)
-        {
-            for(int j=1;j<=n;j++)
-            {
-                dp[i][j]=nums1[i-1]==nums2[j-1] ? 1+dp[i-1][j-1]:0;
-                ans=max(ans,dp[i][j]);
+        int n = nums1.size(), m = nums2.size(), ans = 0;
+        // swap it to ensure n > m
+        if (n < m) {
+            // or you can call findLength(nums2, nums1); 
+            swap(nums1, nums2);
+            swap(n, m);
+        }
+        // dp records current dp state
+        // dp2 records the previous dp state
+        vector<int> dp(n + 1), dp2(n + 1);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    // extend from the previous dp state
+                    dp[j] = dp2[j - 1] + 1; 
+                } else {
+                    // reset to 0
+                    dp[j] = 0;
+                }
+                // record the max length
+                ans = max(ans, dp[j]);
             }
+            // the current state now becomes the previous state for next round
+            dp2 = dp;
         }
         return ans;
-        //TC:- O(N*M)
-    }  
-        
-     /*   Time Complexity : O(M*N), where M is size of A and N is size of B. We have 3 choices at each recursion and max recursion depth is M+N.
-Space Complexity : O(M*N), max depth of recursive stack.
-        */
-    
+    }
 };
