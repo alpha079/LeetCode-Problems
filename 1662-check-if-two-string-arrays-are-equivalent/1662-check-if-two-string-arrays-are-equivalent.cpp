eba5d8ab7455c8606eb7b4826c45cfc9347010b5
Shivam Soni <shivamkmv2311@gmail.com>
2022-10-25 11:56:41 +0530

    Time: 10 ms (44.93%), Space: 11.4 MB (89.94%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+    bool arrayStringsAreEqual(vector<string>& w1, vector<string>& w2) {
         
         
         
-       string s1="";
-        for(auto it: word1)
+     //two pointers solution 
+    
+      int row1=0,row2=0;
+        int col1=0,col2=0;
+        
+        while(row1<w1.size() and row2<w2.size())
         {
-            s1+=it;
+            if(w1[row1][col1++]!=w2[row2][col2++])
+                return false;
+        
+            if(col1>=w1[row1].size())
+            {
+                col1=0;row1++;
+            }
+            if(col2>=w2[row2].size())
+            {
+                col2=0;row2++;
+            }
         }
-         string s2="";
-        for(auto it: word2)
-        {
-            s2+=it;
-        }
-        return s1==s2;
+        
+        return row1==w1.size() and row2==w2.size();
+        
+        //TC:- O(N*sizeoflongestword)
+        //SC:- O(1)
     }
 };
